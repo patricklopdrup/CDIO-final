3ac3d9fa4b031dc4dd172a89161ce990f110d0d1
plopdrup <s185092@student.dtu.dk>
2019-06-11 00:28:44 +0200

    added css på autocomplete dropdown, så den kan ses nu.

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ $(function () {
     var categories = ["c++", "java", "php", "coldfusion", "javascript", "asp", "ruby"];
     $("#category").autocomplete({
         source: categories,
-        minLenght: 0
+        minLenght: 0,
+        autoFocus: true
     }).focus(function () {
         $(this).autocomplete("Search", "");
     })
